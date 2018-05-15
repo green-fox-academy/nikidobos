5527b5b2275f785cb31fd908654c6a6aa4859615
Niki <nikidobos@gmail.com>
2018-05-15 16:11:37 +0200

    Third list task done

---
+++ week-02/day-2/List_03/List_03/Program.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppendLetter
+{
+    public class AppendLetter
+    {
+        private static object appendBuilder;
+
+        public static void Main(string[] args)
+        {
+            var far = new List<string> { "kuty", "macsk", "kacs", "rók", "halacsk" };
+            // Create a method called AppendA() that adds "a" to every string in the far list.
+            // The parameter should be a list.
+
+            Console.WriteLine(AppendA(far));
+            Console.ReadKey();
+            // Expected output: "kutya", "macska", "kacsa", "róka", "halacska"
+        }
+
+        public static StringBuilder AppendA(List<string> far) 
+        {
+            StringBuilder appendABuilder = new StringBuilder();
+        
+            for (int i = 0; i < far.Count ; i++)
+			{
+                appendABuilder.Append($"\"{far[i] + "a"}\", ");
+			}
+
+            return appendABuilder;
+
+
+        }
+
+    }
+}
