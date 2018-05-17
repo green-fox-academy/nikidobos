176b3581e7fd968eee3999a5a4b6bbc4893e928d
Niki <nikidobos@gmail.com>
2018-05-17 13:49:30 +0200

    add colored box task

---
+++ week-02/day-4/Colored Box/FoxDrawTemplate1/MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Media;
+using FoxDrawNameSpace;
+
+namespace FoxDrawTemplate1
+{
+
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var foxDraw = new FoxDraw(canvas);
+            // Draw a box that has different colored lines on each edge.
+
+            foxDraw.StrokeColor(Colors.Green);
+            foxDraw.DrawLine(200, 100, 500, 100);
+            foxDraw.StrokeColor(Colors.Blue);
+            foxDraw.DrawLine(200, 300, 500, 300);
+            foxDraw.StrokeColor(Colors.Red);
+            foxDraw.DrawLine(200, 100, 200, 300);
+            foxDraw.StrokeColor(Colors.Purple);
+            foxDraw.DrawLine(500, 100, 500, 300);
+
+        }
+    }
+
+}
