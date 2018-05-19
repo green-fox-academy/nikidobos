b47013dfb4f3bb541c0180a1f11bdfe06a0de054
Niki <nikidobos@gmail.com>
2018-05-19 15:41:11 +0200

    add cuboid task

---
+++ week-01/day-3/12 - Cuboid/12 - Cuboid/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a program that stores 3 sides of a cuboid as variables (doubles)
+            // The program should write the surface area and volume of the cuboid like:
+            //
+            // Surface Area: 600
+            // Volume: 1000
+
+            double a = 12;
+            double b = 20;
+            double c = 16;
+
+            Console.WriteLine("Surface area: " + (2 * a * b + 2 * b * c + 2 * a * c));
+            Console.WriteLine("Volume: " + (a * b * c));
+            Console.ReadLine();
+        }
+    }
+}
