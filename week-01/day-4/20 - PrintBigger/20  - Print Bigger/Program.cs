bea32836bd07fc3301eecbf0bc9d215a9bc2b9b9
Niki <nikidobos@gmail.com>
2018-05-19 18:46:23 +0200

    add task

---
+++ week-01/day-4/20 - PrintBigger/20  - Print Bigger/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a program that asks for two numbers and prints the bigger one
+
+            Console.WriteLine("Hello! Write two numbers: ");
+            int number1 = int.Parse(Console.ReadLine());
+            int number2 = int.Parse(Console.ReadLine());
+
+            if (number1 > number2)
+            {
+                Console.WriteLine("The bigger one is: " + number1);
+            }
+            else
+            {
+                Console.WriteLine("The bigger one is: " + number2);
+            }
+            Console.ReadLine();
+        }
+    }
+}
