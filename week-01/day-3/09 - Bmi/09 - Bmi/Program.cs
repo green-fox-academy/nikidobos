c2a50461a21ca9255a60a728480abd41307624d9
Niki <nikidobos@gmail.com>
2018-05-19 15:08:09 +0200

    fixed task

--- week-01/day-3/09 - Bmi/09 - Bmi/Program.cs
+++ week-01/day-3/09 - Bmi/09 - Bmi/Program.cs
@@ -7,10 +7,10 @@ namespace GreenFox
         static void Main(string[] args)
         {
             // Print the Body mass index (BMI) based on these values
-            double massInKg = 81.2;
+            double weightInKg = 81.2;
             double heightInM = 1.78;
 
-            double BMI = massInKg / (heightInM * heightInM); 
+            double BMI = weightInKg / (heightInM * heightInM); 
             Console.WriteLine("The BMI = " + BMI.ToString("0.00") );
 
             Console.ReadLine();
