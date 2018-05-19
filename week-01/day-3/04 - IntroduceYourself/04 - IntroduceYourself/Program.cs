cdcaad8ca63f81f7f0a8867168060dbc31eb07b3
Niki <nikidobos@gmail.com>
2018-05-19 14:51:42 +0200

    fixed task

--- week-01/day-3/04 - IntroduceYourself/04 - IntroduceYourself/Program.cs
+++ week-01/day-3/04 - IntroduceYourself/04 - IntroduceYourself/Program.cs
@@ -10,7 +10,26 @@ namespace _04___IntroduceYourself
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            // Write a program that prints a few details to the terminal window about you
+            // It should print each detail to a new line.
+            //  - Your name
+            //  - Your age
+            //  - Your height in meters (as a decimal fraction)
+            //
+            //  Example output:
+            //  John Doe
+            //  31
+            //  1.87
+
+            string name = "Nikolett Tarjáni - Dobos";
+            int age = 26;
+            double height = 1.6;
+
+            Console.WriteLine(name);
+            Console.WriteLine(age);
+            Console.WriteLine(height);
+
             Console.ReadKey();
         }
     }
