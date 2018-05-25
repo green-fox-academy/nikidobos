9238995303e074f32abf70ade4a3b53841729a5e
Niki <nikidobos@gmail.com>
2018-05-25 12:00:43 +0200

    skeleton(SquareGrid)

---
+++ week-03/day-05/Square/Triangles/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using GreenFox;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Triangles
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var foxDraw = new FoxDraw(canvas);
+            // DrawRectangle(foxDraw);
+            Square square = new Square();
+            DrawRectangle(square);
+        }
+
+        public void DrawRectangle(Square square)
+        {
+            square.DrawRectangle();
+        }
+
+    }
+}
---
+++ week-03/day-05/Square/Triangles/Square.cs
@@ -0,0 +1,21 @@
+﻿using GreenFox;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace Triangles
+{
+    public class Square
+    {        
+        public void DrawRectangle(FoxDraw foxDraw)
+        {
+            foxDraw.StrokeColor(Colors.Black);
+            foxDraw.FillColor(Colors.Transparent);
+            foxDraw.DrawRectangle(0.25 * foxDraw.Canvas.Width, 0.25 * foxDraw.Canvas.Height, 0.25 * foxDraw.Canvas.Width, 0.25 * foxDraw.Canvas.Height, 15);
+
+        }
+
+    }
+}
