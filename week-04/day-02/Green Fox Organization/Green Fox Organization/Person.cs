3cecabef4fe808a874a01814de6057e1cee8662f
Niki <nikidobos@gmail.com>
2018-05-29 13:05:41 +0200

    add(GreenFoxOrganization)

---
+++ week-04/day-02/Green Fox Organization/Green Fox Organization/Person.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Green_Fox_Organization
+{
+    class Person
+    {
+        public string name;
+        public int age;
+        public string gender;
+
+        public Person(string name, int age, string gender)
+        {
+            this.name = "Jane Doe";
+            this.age = 30;
+            this.gender = "female";
+        }
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+        }
+
+        public int Age
+        {
+            get
+            {
+                return age;
+            }
+        }
+
+        public string Gender
+        {
+            get
+            {
+                return gender;
+            }
+        }
+
+        public string Introduce(string name, int age, string gender)
+        {
+            return String.Format("Hi, I'm {0}, a {1} year old {2}.", name, age, gender);
+        }
+
+        public string GetGoal()
+        {
+            return String.Format("My goal is: Live for the moment!"); 
+
+        }
+    }
+}
---
+++ week-04/day-02/Green Fox Organization/Green Fox Organization/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Green_Fox_Organization
+{
+    class Student
+    {
+        public string previousOrganization;
+        public int skippedDays;
+
+    }
+}
