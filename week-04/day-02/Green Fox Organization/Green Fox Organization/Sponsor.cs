0de8b46a6c14eac08637cf30aec2bd0c1bebb8a7
Niki <nikidobos@gmail.com>
2018-05-29 13:57:41 +0200

    feat(GreenFoxOrganization) add Sponsor.cs, Introduce(), Hire()

---
+++ week-04/day-02/Green Fox Organization/Green Fox Organization/Sponsor.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Green_Fox_Organization
+{
+    class Sponsor: Person
+    {
+        public string company;
+        public int hiredStudents = 0;
+
+        public Sponsor(string name, int age, string gender, string company) : base (name, age, gender)
+        {
+            this.company = "Bosch";
+        }
+        //        Introduce() : "Hi, I'm name, a age year old gender who represents company and hired hiredStudents students so far."
+        //Hire() : increase hiredStudents by 1
+        //GetGoal() : prints out "Hire brilliant junior software developers."
+
+        public override string Introduce()
+        {
+            return String.Format("Hi, I'm {0}, a {1} year old {2} who represents {3} and hired {4} students so far.", name, age, gender, company, hiredStudents);
+        }
+
+        public void Hire()
+        {
+            hiredStudents++;
+        }
+
+
+    }
+}
