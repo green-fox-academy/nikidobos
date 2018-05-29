a233bc330fd7706ddec8e95e20afd3d777c3a55f
Niki <nikidobos@gmail.com>
2018-05-29 13:32:26 +0200

    feat(GreenFoxOrganization) add constructor to the Mentor class

--- week-04/day-02/Green Fox Organization/Green Fox Organization/Mentor.cs
+++ week-04/day-02/Green Fox Organization/Green Fox Organization/Mentor.cs
@@ -8,5 +8,15 @@ namespace Green_Fox_Organization
 {
     class Mentor: Person
     {
+        private string level;
+
+        public Mentor(string name, int age, string gender, string level) : base(name, age, gender)
+        {
+            this.level = level;
+        }
+
+       // GetGoal() : prints out "Educate brilliant junior software developers."
+        //Introduce() : "Hi, I'm name, a age year old gender level mentor."
+
     }
 }
