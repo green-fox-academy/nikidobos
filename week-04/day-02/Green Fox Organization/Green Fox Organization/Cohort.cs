2223ce84970a895b4c03e285d343dd494897e18a
Niki <nikidobos@gmail.com>
2018-05-29 14:04:44 +0200

    feat(GreenFoxOrganization) made cohort constructor

--- week-04/day-02/Green Fox Organization/Green Fox Organization/Cohort.cs
+++ week-04/day-02/Green Fox Organization/Green Fox Organization/Cohort.cs
@@ -6,7 +6,17 @@ using System.Threading.Tasks;
 
 namespace Green_Fox_Organization
 {
-    class Cohort : Person
+    class Cohort
     {
+        public string name;
+        public List<string> students;
+        public List<string> mentors;
+
+        public Cohort(string name, List<string> students, List<string> mentors)
+        {
+            this.name = "Fulvipes";
+            this.students = students;
+            this.mentors = mentors;
+        }
     }
 }
