b8fbeec369deceba760dd45a6adcda954d6c74a9
Niki <nikidobos@gmail.com>
2018-05-29 14:17:26 +0200

    feat(GreenFoxOrganization) add AddStudent(),AddMentor(), Info()

--- week-04/day-02/Green Fox Organization/Green Fox Organization/Cohort.cs
+++ week-04/day-02/Green Fox Organization/Green Fox Organization/Cohort.cs
@@ -15,8 +15,26 @@ namespace Green_Fox_Organization
         public Cohort(string name, List<string> students, List<string> mentors)
         {
             this.name = "Fulvipes";
-            this.students = students;
-            this.mentors = mentors;
+            List<string> students = new List<string>();
+            List<string> mentors = new List<string>();
+        }
+         //       addStudent(Student) : adds the given Student to students list
+         //addMentor(Mentor): adds the given Mentor to mentors list
+         //info(): prints out "The name cohort has students.size() students and mentors.size() mentors."
+
+        public void AddStudent()
+        {
+            students.Add();
+        }
+
+        public void AddMentor()
+        {
+
+        }
+
+        public string Info()
+        {
+            return String.Format("The {0} cohort has {1} students and {2} mentors.", name, students.Count, mentors.Count);
         }
     }
 }
