ff7ab2a3082c8d4b7514746c51a3f7ac46633880
Niki <nikidobos@gmail.com>
2018-06-18 16:50:03 +0200

    chore(exercise 10) add Fox Class

---
+++ week-06/day-01/10 - Fox/10 - Fox/Fox.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10___Fox
+{
+    class Fox
+    {
+        string name;
+        string type;
+        string color;
+
+        public Fox(string name, string type, string color)
+        {
+            this.Name = name;
+            this.Type = type;
+            this.Color = color;
+        }
+
+        public string Name { get => name; set => name = value; }
+        public string Type { get => type; set => type = value; }
+        public string Color { get => color; set => color = value; }
+    }
+}
