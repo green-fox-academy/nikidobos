708f22ec39cedb0d629a1dcaf6ec112c96098d3c
Niki <nikidobos@gmail.com>
2018-06-18 12:02:21 +0200

    add(exercise 4) add query syntax

---
+++ week-06/day-01/4 - Find Number Squared/4 - Find Number Squared/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4___Find_Number_Squared
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] n = new[] { 3, 9, 2, 8, 6, 5 };
+
+            var value = from number in n
+                        where number * number > 20
+                        select number;
+
+            foreach (var num in value)
+            {
+                Console.WriteLine(num);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
