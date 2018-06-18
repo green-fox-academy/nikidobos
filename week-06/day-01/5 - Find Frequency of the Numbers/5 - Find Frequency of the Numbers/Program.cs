8dc115339b58cf8cdf018e3312e63f40cb717a6f
Niki <nikidobos@gmail.com>
2018-06-18 12:39:21 +0200

    add(exercise 5) add query syntax

---
+++ week-06/day-01/5 - Find Frequency of the Numbers/5 - Find Frequency of the Numbers/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5___Find_Frequency_of_the_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] n = new int[] { 5, 9, 1, 2, 3, 7, 5, 6, 7, 3, 7, 6, 8, 5, 4, 9, 6, 2 };
+
+            var freq = from x in n
+                    group x by x into y
+                    select y;
+            Console.WriteLine("Numbers and frequencies are :");
+            foreach (var item in freq)
+            {
+                Console.WriteLine(item.Key + " appears : " + item.Count() + " times");
+            }
+            Console.ReadLine();
+        }
+    }
+}
