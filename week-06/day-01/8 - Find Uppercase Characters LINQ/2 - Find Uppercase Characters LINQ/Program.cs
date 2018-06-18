cb3f77aa83263869bec5ed4e30617cbef0aa4398
Niki <nikidobos@gmail.com>
2018-06-18 14:07:25 +0200

    add(exercise 8)

---
+++ week-06/day-01/8 - Find Uppercase Characters LINQ/2 - Find Uppercase Characters LINQ/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2___Find_Uppercase_Characters_LINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string toCheck = "Hello World!";
+            IEnumerable<char> numberOfUpperCaseChars = from c in toCheck
+                                         where char.IsUpper(c)
+                                         select c;
+            Console.WriteLine("The uppercase letters in the word: ");
+            foreach (var upperCaseLetters in numberOfUpperCaseChars)
+            {
+                Console.WriteLine(upperCaseLetters);
+            }
+
+            IEnumerable<char> numberOfUpperChars = from c in toCheck
+                                                       where char.IsUpper(c)
+                                                       select c;
+            Console.WriteLine("The uppercase letters in the word: ");
+            foreach (var upperCaseLetters in numberOfUpperChars)
+            {
+                Console.WriteLine(upperCaseLetters);
+            }
+            Console.ReadLine();
+        }
+    }
+}
