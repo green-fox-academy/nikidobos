1d41f31bca27cf34eb83d2ef5d613c6e074e6046
Niki <nikidobos@gmail.com>
2018-06-18 10:58:45 +0200

    add(exercise 2)

---
+++ week-06/day-01/2 - Average Value of Odd Numbers/2 - Average Value of Odd Numbers/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2___Average_Value_of_Odd_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] n = { 1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14 };
+
+            var avg = from number in n
+                       where number % 2 == 1
+                       select number;
+
+            Console.WriteLine("The average of the odd numbers: " + avg.Average());
+
+            var avgOfOddNumbers = n.Where(x => x % 2 == 1).Average();
+
+            Console.WriteLine("The average of the odd numbers: " + avgOfOddNumbers);
+
+            Console.ReadLine();
+        }
+    }
+}
+
