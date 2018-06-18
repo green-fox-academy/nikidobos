c0a9a41f8715329b2f30cf98ab99dc8f9f81d664
Niki <nikidobos@gmail.com>
2018-06-18 13:47:08 +0200

    add(exercise 7) add method syntax

--- week-06/day-01/7 - Find strings starts,ends with givenchar/7 - Find strings starts,ends with givenchar/Program.cs
+++ week-06/day-01/7 - Find strings starts,ends with givenchar/7 - Find strings starts,ends with givenchar/Program.cs
@@ -12,16 +12,21 @@ namespace _7___Find_strings_starts_ends_with_givenchar
         {
             string[] cities = { "ROME", "LONDON", "NAIROBI", "CALIFORNIA", "ZURICH", "NEW DELHI", "AMSTERDAM", "ABU DHABI", "PARIS" };
 
-            IEnumerable<string> startsends = from word in cities
+            IEnumerable<string> startsEnds = from word in cities
                                              where word.StartsWith("A") && word.EndsWith("I")
                                              select word;
 
-            foreach (var item in startsends)
+            foreach (var item in startsEnds)
+            {
+                Console.WriteLine(item);
+            }
+
+            IEnumerable<string> startsEndsWithGivenChar = cities.Where(word => word.StartsWith("A") && word.EndsWith("I"));
+            foreach (var item in startsEndsWithGivenChar)
             {
                 Console.WriteLine(item);
             }
             Console.ReadLine();
         }
-
     }
 }
