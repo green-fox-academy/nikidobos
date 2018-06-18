a6c149dd96620b7b3b074f2deaae9f65e9554cc3
Niki <nikidobos@gmail.com>
2018-06-18 11:45:47 +0200

    fix(exercise 2) fix Boolean expression

--- week-06/day-01/2 - Average Value of Odd Numbers/2 - Average Value of Odd Numbers/Program.cs
+++ week-06/day-01/2 - Average Value of Odd Numbers/2 - Average Value of Odd Numbers/Program.cs
@@ -14,12 +14,12 @@ namespace _2___Average_Value_of_Odd_Numbers
             int[] n = { 1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14 };
 
             var avg = from number in n
-                       where number % 2 == 1
+                       where number % 2 != 0
                        select number;
 
             Console.WriteLine("The average of the odd numbers: " + avg.Average());
 
-            var avgOfOddNumbers = n.Where(x => x % 2 == 1).Average();
+            var avgOfOddNumbers = n.Where(x => x % 2 != 0).Average();
 
             Console.WriteLine("The average of the odd numbers: " + avgOfOddNumbers);
 
