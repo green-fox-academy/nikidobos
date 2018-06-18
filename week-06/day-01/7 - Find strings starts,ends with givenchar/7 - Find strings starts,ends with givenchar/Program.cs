9acdfc1a6993cc49d4297862dcd5d02213919747
Niki <nikidobos@gmail.com>
2018-06-18 13:20:38 +0200

    add(exercise 7) add query syntax

---
+++ week-06/day-01/7 - Find strings starts,ends with givenchar/7 - Find strings starts,ends with givenchar/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7___Find_strings_starts_ends_with_givenchar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] cities = { "ROME", "LONDON", "NAIROBI", "CALIFORNIA", "ZURICH", "NEW DELHI", "AMSTERDAM", "ABU DHABI", "PARIS" };
+
+            IEnumerable<string> startsends = from word in cities
+                                             where word.StartsWith("A") && word.EndsWith("I")
+                                             select word;
+
+            foreach (var item in startsends)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadLine();
+        }
+
+    }
+}
