c3efc76a13c23ed1696d390608dcd6d0c58efd8f
Niki <nikidobos@gmail.com>
2018-06-25 16:02:48 +0200

    add(HelloWorldWebApp) add Views

--- week-07/day-01/HelloWorldWebApp/HelloWorldWebApp/Controllers/WebController.cs
+++ week-07/day-01/HelloWorldWebApp/HelloWorldWebApp/Controllers/WebController.cs
@@ -2,18 +2,26 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using HelloWorldWebApp.Models;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace HelloWorldWebApp.Controllers
 {
+    [Route("web")]
     public class WebController : Controller
     {
-        // GET: /<controller>/
-        public IActionResult Index()
+        [Route("greeting")]
+        public IActionResult Greeting()
         {
-            return View();
+            var greeting = new Greeting()
+            {
+                Id = 1,
+                Content = "World"
+            };
+
+            return View(greeting);
         }
     }
 }
