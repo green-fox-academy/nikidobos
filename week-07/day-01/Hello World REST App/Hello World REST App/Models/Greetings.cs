73f4d663101e964a5eca36a3351d997843f5684e
Niki <nikidobos@gmail.com>
2018-06-25 12:12:27 +0200

    chore(HelloWorldREST)

---
+++ week-07/day-01/Hello World REST App/Hello World REST App/Models/Greetings.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hello_World_REST_App.Models
+{
+    public class Greetings
+    {
+        long id;
+        string Content;
+
+        public long Id { get => id; set => id = value; }
+        public string Content1 { get => Content; set => Content = value; }
+    }
+}
---
+++ week-07/day-01/Hello World REST App/Hello World REST App/RESTController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Hello_World_REST_App
+{
+    public class RESTController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
