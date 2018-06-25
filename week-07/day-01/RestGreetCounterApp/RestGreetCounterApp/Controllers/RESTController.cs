eace4dce253c867475aea18777c2f63abef7b4b2
Niki <nikidobos@gmail.com>
2018-06-25 15:36:45 +0200

    fix(RestGreetCounter) deleted unnecessary methot from Greeting class, fixed counter

---
+++ week-07/day-01/RestGreetCounterApp/RestGreetCounterApp/Controllers/RESTController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using RestGreetCounterApp.Models;
+
+namespace RestGreetCounterApp.Controllers
+{
+    [Route("api")]
+    public class RESTController : Controller
+    {
+        static int numberToCount = 0;
+        [Route("greeting")]
+        public IActionResult Greeting(string name)
+        {
+            numberToCount++;
+            Greeting greeting = new Greeting() { Id = numberToCount, Content = "Hello, " + name };
+            return new JsonResult(greeting);
+        }
+    }
+}
---
+++ week-07/day-01/RestGreetCounterApp/RestGreetCounterApp/Models/Greeting.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestGreetCounterApp.Models
+{
+    public class Greeting
+    {
+        static long id;
+        string content;
+
+        public long Id { get => id; set => id = value; }
+        public string Content { get => content; set => content = value; }
+    }
+}
