7c3516868c0d2fcaf02d2618622cf0bb63f1e527
Niki <nikidobos@gmail.com>
2018-06-25 14:49:42 +0200

    chore(RestGreetCounter)

---
+++ week-07/day-01/HelloUserRestApp/HelloUserRestApp/Controllers/RestController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HelloUserRestApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HelloUserRestApp.Controllers
+{
+    [Route("api")]
+    public class RestController : Controller
+    {
+        [Route("greeting")]
+        public IActionResult Greeting(string name)
+        {
+            Greeting greeting = new Greeting() { Id = 1, Content = "Hello, " + name };
+            return new JsonResult(greeting);
+        }
+    }
+}
---
+++ week-07/day-01/HelloUserRestApp/HelloUserRestApp/Models/Greeting.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HelloUserRestApp.Models
+{
+    public class Greeting
+    {
+        long id;
+        string content;
+
+        public long Id { get => id; set => id = value; }
+        public string Content { get => content; set => content = value; }
+    }
+}
