9f27e5a0d4ef8164243df6074cea3aa721296837
Niki <nikidobos@gmail.com>
2018-06-27 12:02:28 +0200

    add(Counter) CounterService

---
+++ week-07/day-03/Counter/Counter/Services/CounterService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Counter.Services
+{
+    public class CounterService : ICounter
+    {
+        public int GetNumber()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Increase()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
