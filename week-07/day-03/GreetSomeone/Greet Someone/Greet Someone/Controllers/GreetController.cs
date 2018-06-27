ad23378fc1c7e8e2e325a87b5899f35708775729
Niki <nikidobos@gmail.com>
2018-06-27 14:48:39 +0200

    chore(GreetSomeone)

---
+++ week-07/day-03/GreetSomeone/Greet Someone/Greet Someone/Controllers/GreetController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Greet_Someone.Controllers
+{
+    public class GreetController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ week-07/day-03/GreetSomeone/Greet Someone/Greet Someone/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Greet_Someone.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
