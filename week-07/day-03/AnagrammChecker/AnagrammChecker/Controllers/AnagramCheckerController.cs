ec201a12b662aaaa92606b1f59f1993658dc6dfe
Niki <nikidobos@gmail.com>
2018-06-27 21:29:46 +0200

    chore(AnagramChecker)

---
+++ week-07/day-03/AnagrammChecker/AnagrammChecker/Controllers/AnagramCheckerController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AnagrammChecker.Controllers
+{
+    public class AnagramCheckerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult GiveWords(string wordToCheck1, string wordToCheck2)
+        {
+            return View();
+        }
+    }
+}
