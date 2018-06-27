9e70422ebc2b7c18d231bfc3585624dbf54a3544
Niki <nikidobos@gmail.com>
2018-06-27 14:58:37 +0200

    add(GreetSomeone) Index View

--- week-07/day-03/GreetSomeone/Greet Someone/Greet Someone/Controllers/GreetController.cs
+++ week-07/day-03/GreetSomeone/Greet Someone/Greet Someone/Controllers/GreetController.cs
@@ -12,5 +12,11 @@ namespace Greet_Someone.Controllers
         {
             return View();
         }
+
+        [HttpGet("/")]
+        public IActionResult GetForm()
+        {
+            return View();
+        }
     }
 }
