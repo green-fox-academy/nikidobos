315e6a921e36a22903de293b76bf0d52021789f8
Niki <nikidobos@gmail.com>
2018-06-27 12:07:53 +0200

    fix(Counter) fix methods in Controller

--- week-07/day-03/Counter/Counter/Controllers/CounterController.cs
+++ week-07/day-03/Counter/Counter/Controllers/CounterController.cs
@@ -2,12 +2,20 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Counter.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Counter.Controllers
 {
     public class CounterController : Controller
     {
+        private ICounter counterService;
+
+        public CounterController(ICounter counterService)
+        {
+            this.counterService = counterService;
+        }
+
         private int number = 0;
         [HttpGet]
         [Route("/")]
