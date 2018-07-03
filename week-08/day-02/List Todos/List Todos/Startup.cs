1a1fe530fa00c35821debe3f9170ca93c02f3e9a
Niki <nikidobos@gmail.com>
2018-07-03 14:53:23 +0200

    feat(List Todos) add connection

--- week-08/day-02/List Todos/List Todos/Startup.cs
+++ week-08/day-02/List Todos/List Todos/Startup.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using List_Todos.Repositories;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -21,7 +23,10 @@ namespace List_Todos
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            string connectionString = @"Data Source = (localdb)\MSSQLLocalDB; Initial Catalog = todos; Integrated Security = True; Connect Timeout = 30; Encrypt = False; TrustServerCertificate = False; ApplicationIntent = ReadWrite; MultiSubnetFailover = False"
             services.AddMvc();
+            services.AddDbContext<TodoDbContext>(options => options.UseSqlServer(connectionString));
+            services.AddTransient<TodoDbContext>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
