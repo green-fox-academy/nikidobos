8e7b49dd8869c473db5d4d421f3763ebe42ee2e0
Niki <nikidobos@gmail.com>
2018-07-03 13:52:45 +0200

    add(List Todos) add properties to Todo class

---
+++ week-08/day-02/List Todos/List Todos/Models/Todo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace List_Todos.Models
+{
+    public class Todo
+    {
+        private long Id { set; get; }
+        private string Title { set; get; }
+        private bool IsUrgent { set; get; }
+        private bool IsDone { set; get; }
+    }
+}
