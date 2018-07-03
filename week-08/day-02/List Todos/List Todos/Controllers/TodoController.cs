fc5c384d0be32353a8f55ad9cec7b69533c7169c
Niki <nikidobos@gmail.com>
2018-07-03 13:57:28 +0200

    add(List Todos) add TodoController

---
+++ week-08/day-02/List Todos/List Todos/Controllers/TodoController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace List_Todos.Controllers
+{
+    public class TodoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet("/")]
+        public IActionResult List()
+        {
+            return Content("This is my first todo");
+        }
+    }
+}
