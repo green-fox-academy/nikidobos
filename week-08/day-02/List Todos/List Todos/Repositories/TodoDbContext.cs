de376ba0998c1977b9a25d642453e8dca55a859e
Niki <nikidobos@gmail.com>
2018-07-03 14:26:24 +0200

    feat(List Todos) add TodoDbContext

---
+++ week-08/day-02/List Todos/List Todos/Repositories/TodoDbContext.cs
@@ -0,0 +1,18 @@
+﻿using List_Todos.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace List_Todos.Repositories
+{
+    public class TodoDbContext : DbContext
+    {
+        public DbSet<Todo> todos { get; set; }
+        public TodoDbContext(DbContextOptions<TodoDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
