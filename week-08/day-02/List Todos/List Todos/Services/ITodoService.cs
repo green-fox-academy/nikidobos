5aa846169e652162baa999f20da0c84d7e3ff1c2
Niki <nikidobos@gmail.com>
2018-07-05 16:36:27 +0200

    feat(List Todos) add ITodoService Interface

---
+++ week-08/day-02/List Todos/List Todos/Services/ITodoService.cs
@@ -0,0 +1,18 @@
+﻿using List_Todos.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace List_Todos.Services
+{
+    public interface ITodoService
+    {
+        //CRUD
+        void AddNewTodo(Todo todo);
+        List<Type> GetAllTodos();
+        void EditTodo(Todo todo);
+        void RemoveTodo(long id);
+        List<Type> GetTodoById(long id);
+    }
+}
---
+++ week-08/day-02/List Todos/List Todos/Services/TodoService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace List Todos.Services
+{
+    public class TodoService
+    {
+    }
+}
