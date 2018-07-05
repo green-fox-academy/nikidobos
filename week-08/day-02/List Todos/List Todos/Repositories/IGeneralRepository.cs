72466bda5c667bc2618173cd67de61dbb0ea4084
Niki <nikidobos@gmail.com>
2018-07-05 15:29:39 +0200

    feat(List Todos) add IGeneralRepository

---
+++ week-08/day-02/List Todos/List Todos/Repositories/IGeneralRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace List_Todos.Repositories
+{
+    public interface IGeneralRepository
+    {
+        //CRUD
+        void Create(Type element);
+        List<Type> GetAllElements();
+        void Update(Type element);
+        void Delete(Type element);
+    }
+}
