26fd3bbb3c2fa4509e876acfe9377a2ea3867db8
Niki <nikidobos@gmail.com>
2018-07-05 14:55:48 +0200

    feat(List Todos) delete empty lines

--- week-08/day-02/List Todos/List Todos/Repositories/TodoRepository.cs
+++ week-08/day-02/List Todos/List Todos/Repositories/TodoRepository.cs
@@ -40,15 +40,8 @@ namespace List_Todos.Repositories
 
         public void EditTodo(Todo todo)
         {
-            //var toEdit = GetTodoById(idToEdit);
             todoDbContext.Update(todo);
             todoDbContext.SaveChanges();
-            //if (toEdit != null)
-            //{
-            //    //toEdit.Title = todo.Title;
-            //    //toEdit.IsUrgent = todo.IsUrgent;
-            //    //toEdit.IsDone = todo.IsDone;
-            //}
         }
 
         public Todo GetTodoById(long idToFind)
