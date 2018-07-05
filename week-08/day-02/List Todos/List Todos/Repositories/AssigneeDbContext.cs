a2dc9aae94cb96d6c2d53a7d022d7ac9c8a76a72
Niki <nikidobos@gmail.com>
2018-07-05 15:01:41 +0200

    feat(List Todos) add AssigneeDbContext

---
+++ week-08/day-02/List Todos/List Todos/Repositories/AssigneeDbContext.cs
@@ -0,0 +1,18 @@
+﻿using List_Todos.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace List_Todos.Repositories
+{
+    public class AssigneeDbContext : DbContext
+    {
+        public DbSet<Assignee> Assignees { get; set; }
+        public AssigneeDbContext(DbContextOptions<AssigneeDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
