14f75e42e91753e45fb77908763fc74d2e60fc8d
Niki <nikidobos@gmail.com>
2018-07-05 14:39:07 +0200

    feat(List Todos) add Addignee model

---
+++ week-08/day-02/List Todos/List Todos/Models/Assignee.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace List_Todos.Models
+{
+    public class Assignee
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+    }
+}
