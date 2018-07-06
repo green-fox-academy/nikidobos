bb979fda9669f8a1f12057b968fcc25322a2c636
Niki <nikidobos@gmail.com>
2018-07-06 10:40:41 +0200

    feat(Reddit) rename the model, add DbContext

---
+++ week-08/day-05/Reddit/Reddit/Repository/RedditDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Reddit.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Reddit.Repository
+{
+    public class RedditDbContext : DbContext
+    {
+        public DbSet<Post> Posts { get; set; }
+        public RedditDbContext(DbContextOptions<RedditDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
