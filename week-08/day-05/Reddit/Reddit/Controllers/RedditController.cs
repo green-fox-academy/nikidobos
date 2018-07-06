edb6d33eb54a83f90bb499a6440544946dce28b8
Niki <nikidobos@gmail.com>
2018-07-06 11:46:34 +0200

    feat(Reddit) add RedditController

---
+++ week-08/day-05/Reddit/Reddit/Controllers/RedditController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Reddit.Services;
+
+namespace Reddit.Controllers
+{
+    public class RedditController : Controller
+    {
+        public RedditService service;
+        public RedditController(RedditService service)
+        {
+            this.service = service;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }        
+    }
+}
