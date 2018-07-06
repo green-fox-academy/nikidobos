7cbb603067729c39bfb426d5d2869d672027c086
Niki <nikidobos@gmail.com>
2018-07-06 10:36:16 +0200

    feat(Reddit) add redditmodell

---
+++ week-08/day-05/Reddit/Reddit/Models/Reddit.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Reddit.Models
+{
+    public class Reddit
+    {
+        public int Id { get; set; }
+        public int NumberOfVotes { get; set; }
+        public string Post { get; set; }
+    }
+}
