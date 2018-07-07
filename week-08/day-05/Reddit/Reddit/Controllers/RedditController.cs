084147f4a732ade30ac1aaa7bdb64d083228a460
Niki <nikidobos@gmail.com>
2018-07-07 16:04:40 +0200

    feat(Reddit) add Submit button to the Listview,  created two () in controller

--- week-08/day-05/Reddit/Reddit/Controllers/RedditController.cs
+++ week-08/day-05/Reddit/Reddit/Controllers/RedditController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Reddit.Models;
 using Reddit.Services;
 
 namespace Reddit.Controllers
@@ -36,6 +37,19 @@ namespace Reddit.Controllers
         public IActionResult DecreaseVote(int id)
         {
             service.DecreaseVote(id);
+            return RedirectToAction("List");
+        }
+
+        [HttpGet("/submit")]
+        public IActionResult AddNewPost()
+        {
+            return View();
+        }
+
+        [HttpPost("/submit")]
+        public IActionResult AddNewPost(Post post)
+        {
+            service.AddNewPost(post);
             return RedirectToAction("List");
         }
     }
