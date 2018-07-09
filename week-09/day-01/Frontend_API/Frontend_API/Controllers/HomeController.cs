3a087ab072d10b91b1f6abb1649b65386d62dbc9
Niki <nikidobos@gmail.com>
2018-07-09 13:03:04 +0200

    add(Frontend_API) HomeController

---
+++ week-09/day-01/Frontend_API/Frontend_API/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Frontend_API.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Home")]
+    public class HomeController : Controller
+    {
+        [HttpGet("/")]
+        public IActionResult Index()
+        {
+            return File("index.html", "text/html");
+        }
+    }
+}
