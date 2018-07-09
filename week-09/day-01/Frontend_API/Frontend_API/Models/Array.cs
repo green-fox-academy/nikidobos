475fc27171bb723066beeca663dcb00de0ba02e9
Niki <nikidobos@gmail.com>
2018-07-09 19:24:42 +0200

    add(Frontend_API) add Array model

---
+++ week-09/day-01/Frontend_API/Frontend_API/Models/Array.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Frontend_API.Models
+{
+    public class Array
+    {
+        public int[] toModify { get; set; }
+    }
+}
