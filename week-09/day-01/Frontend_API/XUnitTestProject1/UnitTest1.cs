bb57fd00c6e07aa8904afbb4f303668430c0f0ce
Niki <nikidobos@gmail.com>
2018-07-10 11:59:53 +0200

    add(Frontend_API) add client, server to the test

--- week-09/day-01/Frontend_API/XUnitTestProject1/UnitTest1.cs
+++ week-09/day-01/Frontend_API/XUnitTestProject1/UnitTest1.cs
@@ -1,10 +1,23 @@
+using Frontend_API;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
 using System;
+using System.Net.Http;
 using Xunit;
 
 namespace XUnitTestProject1
 {
     public class UnitTest1
     {
+        private HttpClient Client { get; set; }
+        private TestServer Server { get; set; }
+
+        public UnitTest1()
+        {
+            Server = new TestServer(new WebHostBuilder().UseStartup<Startup>());
+            Client = Server.CreateClient();
+        }
+
         [Fact]
         public void Test1()
         {
