1b52ce4f3da2742031f8682a8607827dc4e0beb1
Niki <nikidobos@gmail.com>
2018-07-10 21:55:27 +0200

    feat(Reddit) add DI, modified the View

--- week-08/day-05/Reddit/Reddit/Startup.cs
+++ week-08/day-05/Reddit/Reddit/Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Reddit.Repository;
 using Reddit.Services;
+using Reddit.ViewModels;
 
 namespace Reddit
 {
@@ -32,7 +33,7 @@ namespace Reddit
             services.AddTransient<IRedditService, RedditService>();
             services.AddTransient<UserRepository>();
             services.AddTransient<UserService>();
-
+            services.AddTransient<RedditViewModel>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
