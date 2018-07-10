b6e87c0d4f0d62781bc56d4a3936d163d543c145
Niki <nikidobos@gmail.com>
2018-07-10 21:49:36 +0200

    feat(Reddit) add RedditViewModel

---
+++ week-08/day-05/Reddit/Reddit/ViewModels/RedditViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Reddit.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Reddit.ViewModels
+{
+    public class RedditViewModel
+    {
+        public List<Post> Posts { get; set; }
+        public User User { get; set; }
+    }
+}
