db60fcf845f29793da361fc222f2042dbfc166eb
Niki <nikidobos@gmail.com>
2018-07-10 21:14:48 +0200

    feat(Reddit) add UserService

---
+++ week-08/day-05/Reddit/Reddit/Services/UserService.cs
@@ -0,0 +1,43 @@
+﻿using Reddit.Models;
+using Reddit.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Reddit.Services
+{
+    public class UserService
+    {
+        public UserRepository userRepository;
+        public UserService(UserRepository userRepository)
+        {
+            this.userRepository = userRepository;
+        }
+
+        public void AddNewUser(User user)
+        {
+            userRepository.Create(user);
+        }
+
+        public void DeleteUser(User user)
+        {
+            userRepository.Delete(user);
+        }
+
+        public List<User> GetAllUsers()
+        {
+            return userRepository.Read();
+        }
+
+        public User GetUserById(int id)
+        {
+            return userRepository.GetElementById(id);
+        }
+
+        public void UpdateUser(User user)
+        {
+            userRepository.Update(user);
+        }
+    }
+}
