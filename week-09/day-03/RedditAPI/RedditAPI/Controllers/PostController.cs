98a672da7af0162227c43ea2ee4d63f3ade3f08e
Niki <nikidobos@gmail.com>
2018-07-11 11:44:46 +0200

    fix (RedditAPI) add List() to controller

--- week-09/day-03/RedditAPI/RedditAPI/Controllers/PostController.cs
+++ week-09/day-03/RedditAPI/RedditAPI/Controllers/PostController.cs
@@ -3,15 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Reddit.Services;
 
 namespace RedditAPI.Controllers
 {
     [Produces("application/json")]
     public class HomeController : Controller
     {
+        private RedditService service;
+        public HomeController(RedditService service)
+        {
+            this.service = service;
+        }
+
         public IActionResult Index()
         {
             return View();
         }
+
+        [HttpGet("list")]
+        public IActionResult List()
+        {
+            return Json(new { });
+        }
+
     }
 }
---
+++ week-09/day-03/RedditAPI/RedditAPI/Services/RedditService.cs
@@ -0,0 +1,70 @@
+﻿using Reddit.Models;
+using Reddit.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Reddit.Services
+{
+    public class RedditService : IRedditService
+    {
+        public RedditRepository redditRepository;
+        public UserRepository userRepository;
+        public RedditService(RedditRepository redditRepository, UserRepository userRepository)
+        {
+            this.redditRepository = redditRepository;
+            this.userRepository = userRepository;
+        }
+
+        public void AddNewPost(Post post)
+        {
+            post.DateOfPost = DateTime.Now.ToString("yyyy.MM.dd H:mm");
+            redditRepository.Create(post);
+        }
+
+        public void IncreaseVote(int id)
+        {
+            var forVoting = GetPostById(id);
+            forVoting.NumberOfVotes++;
+            redditRepository.Update(forVoting);
+        }
+
+        public void DeletePost(Post post)
+        {
+            redditRepository.Delete(post);
+        }
+
+        public List<Post> GetAllPosts()
+        {
+            return redditRepository.Read();
+        }
+
+        public Post GetPostById(int id)
+        {
+            return redditRepository.GetElementById(id);
+        }
+
+        public void UpdatePost(Post post)
+        {
+            redditRepository.Update(post);
+        }
+
+        public void DecreaseVote(int id)
+        {
+            var forVoting = GetPostById(id);
+            forVoting.NumberOfVotes--;
+            redditRepository.Update(forVoting);
+        }
+
+        public List<User> GetAlUsers()
+        {
+            return userRepository.Read();
+        }
+
+        public User GetUserById(int id)
+        {
+            return userRepository.GetElementById(id);
+        }
+    }
+}
