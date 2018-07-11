8a3ed281f44b988f905d6ff7b4cd7ab52f925de3
Niki <nikidobos@gmail.com>
2018-07-11 10:26:56 +0200

    feat(RedditAPI) add IGenericRepository,RedditRepo

---
+++ week-09/day-03/RedditAPI/RedditAPI/Repositories/IGenericRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace RedditAPI.Repositories
+{
+    public interface IGenericRepository<Type>
+    {
+        void Create(Type element);
+        List<Type> Read();
+        void Update(Type element);
+        void Delete(Type element);
+    }
+}
---
+++ week-09/day-03/RedditAPI/RedditAPI/Repositories/RedditRepository.cs
@@ -0,0 +1,31 @@
+﻿using RedditAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RedditAPI.Repositories
+{
+    public class RedditRepository : IGenericRepository<Post>
+    {
+        public void Create(Post post)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Post post)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Post> Read()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Post post)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
