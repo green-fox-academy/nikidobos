84250c0f450d598b4288eb7ac339ab737e6d3511
Niki <nikidobos@gmail.com>
2018-07-31 22:03:15 +0200

    feat(TodoApp) modify the controller, add view

--- week-10/TodoApp/src/main/java/com/todos/todo/controller/TodoController.java
+++ week-10/TodoApp/src/main/java/com/todos/todo/controller/TodoController.java
@@ -3,10 +3,19 @@ package com.todos.todo.controller;
 import com.todos.todo.service.TodoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Map;
 
 @Controller
 public class TodoController {
 
   @Autowired
   private TodoService service;
+
+  @RequestMapping("/")
+  public String todos(Map<String, Object> model){
+    return "alltodos";
+  }
+
 }
