1f2eec287609d871ada546aab6042217e8041abb
Niki <nikidobos@gmail.com>
2018-07-31 21:55:18 +0200

    feat(TodoApp) add addNewTodo() to service

--- week-10/TodoApp/src/main/java/com/todos/todo/service/TodoService.java
+++ week-10/TodoApp/src/main/java/com/todos/todo/service/TodoService.java
@@ -1,7 +1,17 @@
 package com.todos.todo.service;
 
+import com.todos.todo.model.Todo;
+import com.todos.todo.repository.TodoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TodoService {
+
+  @Autowired
+  private TodoRepository repository;
+
+  public void addNewTodo(Todo todo){
+    repository.save(todo);
+  }
 }
