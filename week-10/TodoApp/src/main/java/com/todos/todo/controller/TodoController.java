93e14ca30184259949ca454ac1880ffd2390cd56
Niki <nikidobos@gmail.com>
2018-08-01 20:02:50 +0200

    feat(TodoApp) modify the controller

--- week-10/TodoApp/src/main/java/com/todos/todo/controller/TodoController.java
+++ week-10/TodoApp/src/main/java/com/todos/todo/controller/TodoController.java
@@ -1,8 +1,11 @@
 package com.todos.todo.controller;
 
+import com.todos.todo.model.Todo;
 import com.todos.todo.service.TodoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Map;
@@ -16,7 +19,18 @@ public class TodoController {
   @RequestMapping("/")
   public String todos(Map<String, Object> model){
     model.put("title", "feed the monkey");
-    return "alltodos";
+    return "list";
   }
 
+  @GetMapping("/addtodo")
+  public String addTodo(Map<String, Object> model){
+    service.getAllTodos();
+    return "addtodo";
+  }
+
+  @PostMapping("/addtodo")
+  public String addTodo(Map<String, Object> model, Todo todo){
+    service.addNewTodo(todo);
+    return "list";
+  }
 }
--- week-10/TodoApp/src/main/java/com/todos/todo/model/Todo.java
+++ week-10/TodoApp/src/main/java/com/todos/todo/model/Todo.java
@@ -8,16 +8,16 @@ import java.util.Date;
 public class Todo {
 
   @Id
-  private long id;
+  private Long id;
   private String title;
   private boolean urgent;
   private boolean done;
 
-  public long getId() {
+  public Long getId() {
     return id;
   }
 
-  public void setId(long id) {
+  public void setId(Long id) {
     this.id = id;
   }
 
