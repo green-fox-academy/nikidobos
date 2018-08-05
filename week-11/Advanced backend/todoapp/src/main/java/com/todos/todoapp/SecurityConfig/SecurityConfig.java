0db8fcd854a249ea544339f5dff4681b839b7511
Niki <nikidobos@gmail.com>
2018-08-05 18:54:14 +0200

    feat(TodoApp) add configure()

--- week-11/Advanced backend/todoapp/src/main/java/com/todos/todoapp/SecurityConfig/SecurityConfig.java
+++ week-11/Advanced backend/todoapp/src/main/java/com/todos/todoapp/SecurityConfig/SecurityConfig.java
@@ -1,9 +1,18 @@
 package com.todos.todoapp.SecurityConfig;
 
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+  @Override
+  public void configure(HttpSecurity httpSecurity) throws Exception {
+    httpSecurity.authorizeRequests()
+            .antMatchers("/rest").hasRole("USER")
+        .and()
+            .formLogin();
+  }
 }
