51147358b66bf18763750ec20d1793eddee8c892
Niki <nikidobos@gmail.com>
2018-09-02 10:52:50 +0200

    Food Allergy Tracker init

---
+++ Java Project/src/main/java/com/nikidobos/foodallergytracker/FoodallergytrackerApplication.java
@@ -0,0 +1,12 @@
+package com.nikidobos.foodallergytracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodallergytrackerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(FoodallergytrackerApplication.class, args);
+  }
+}
---
+++ Java Project/src/test/java/com/nikidobos/foodallergytracker/FoodallergytrackerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.nikidobos.foodallergytracker;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FoodallergytrackerApplicationTests {
+
+  @Test
+  public void contextLoads() {
+  }
+
+}
