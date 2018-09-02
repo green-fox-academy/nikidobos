e51e73e302aad390e89f0fa920e8d9f42c0e1f72
Niki <nikidobos@gmail.com>
2018-09-02 11:11:45 +0200

    feat(FoodAllergyTracker): add Client model, change application properties

---
+++ Java Project/src/main/java/com/nikidobos/foodallergytracker/Client/Client.java
@@ -0,0 +1,26 @@
+package com.nikidobos.foodallergytracker.Client;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Date;
+
+@Entity
+@Getter
+@Setter
+public class Client {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Long id;
+  private String firstName;
+  private String lastName;
+  private String location;
+  private Date dateOfBirth;
+  private Date dateOfRegistration;
+}
+
