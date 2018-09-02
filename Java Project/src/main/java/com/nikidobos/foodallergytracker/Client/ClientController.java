6dceb793536347c0735ec23147cce5aeaa1b04f7
Niki <nikidobos@gmail.com>
2018-09-02 15:21:09 +0200

    feat(FoodAllergyTracker): add addNewClient(), and the template for it

--- Java Project/src/main/java/com/nikidobos/foodallergytracker/Client/ClientController.java
+++ Java Project/src/main/java/com/nikidobos/foodallergytracker/Client/ClientController.java
@@ -2,6 +2,7 @@ package com.nikidobos.foodallergytracker.Client;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ClientController {
@@ -11,6 +12,11 @@ public class ClientController {
   @Autowired
   public ClientController(ClientService clientService) {
     this.clientService = clientService;
+  }
+
+  @GetMapping("/clients/new")
+  public String addNewClient(){
+    return "addnewclient";
   }
 }
 
